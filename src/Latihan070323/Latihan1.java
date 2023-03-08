7fd915a1978285214d70b30c2ae1ad970ae59ade
RahmahRamadhani <127211226+RahmahRamadhani@users.noreply.github.com>
2023-03-08 13:24:56 +0700

    Initial commit

---
+++ src/Latihan070323/Latihan1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan1 {
+    public static void main(String[] args){
+       int Number = 10;
+       char Letter = 'a';
+       boolean result = true;
+       String str = "hello";
+       
+       System.out.println("Number = "+Number);
+       System.out.println("Letter = "+Letter);
+       System.out.println("result = "+result);
+       System.out.println("str = "+str);
+       
+    }
+}
---
+++ src/Latihan070323/Latihan2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan070323;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Latihan2 {
+    public static void main(String[] args){
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 45;
+        System.out.println("number 1 = "+number1);
+        System.out.println("number 2 = "+number2);
+        System.out.println("number 3 = "+number3);
+        System.out.println("average = "+(number1+number2+number3)/3);
+    }
+}
